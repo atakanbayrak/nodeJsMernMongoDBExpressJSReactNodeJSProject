0c2b2fc17a1bada4d1bfd3d3a205d1adcc42c024
Atakan Bayrak <atakanbayrak461@hotmail.com>
2023-07-11 22:27:49 +0300

    first hierarchy

---
+++ server/config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const database = () => {
+
+        mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser:true,
+            useUnifiedTopology:true
+        }).then(() => {
+            console.log("MongoDB connected")
+        }).catch((err) =>{
+            console.log(err)
+        })
+}
+
+
+module.exports = database 
