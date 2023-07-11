e94932b6ad0cf35ddb1f028bad19ec45addd78ab
Atakan Bayrak <atakanbayrak461@hotmail.com>
2023-07-12 01:01:06 +0300

    second

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+      </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
