f54bc18f7cc39bc73078c1e97dec29e5f225a76e
Atakan Bayrak <atakanbayrak461@hotmail.com>
2023-07-13 13:25:33 +0300

    1.18.03

--- server/index.js
+++ server/index.js
@@ -2,14 +2,21 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
+const database = require('./config/database.js')
+const authRouter = require('./routes/auth.js')
+
+dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json({limit:'30mb',extended:true}))
 app.use(bodyParser.urlencoded({limit:'30mb',extended:true}))
+app.use('/', authRouter)
 
-const PORT = 5000  
+const PORT = process.env.PORT || 5000  
 
+
+database()
 app.listen(PORT, () => {
     console.log("server is running", PORT)
 })
